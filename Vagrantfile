0542d6efecf51e3f895ff0f7b6296995aa8b3a8a
Joshua M. Dotson <josdotso@cisco.com>
2019-04-03 14:29:57 -0400

    Refactor to put NAT64 and DNS64 on Vagrant box instead of LXD

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,19 @@
 
 Vagrant.configure("2") do |config|
 
+  # Install plugins.
+#  required_plugins = %w( vagrant-cachier, vagrant-proxyconf, vagrant-disksize )
+#  _retry = false
+#  required_plugins.each do |plugin|
+#    unless Vagrant.has_plugin? plugin
+#      system "vagrant plugin install #{plugin}"
+#      _retry=true
+#    end
+#  end
+#  if (_retry)
+#    exec "vagrant " + ARGV.join(' ')
+#  end
+
   config.vm.box = "bento/ubuntu-18.04"
 
   config.vm.network "public_network",
@@ -13,6 +26,10 @@ Vagrant.configure("2") do |config|
   config.vm.provider "virtualbox" do |vb|
     vb.cpus = "2"
     vb.memory = "2048"
+
+    ## Enable promiscuous mode on NICs to permit macvlan bridges.
+    #vb.customize ["modifyvm", :id, "--nicpromisc1", "allow-all"]
+    #vb.customize ["modifyvm", :id, "--nicpromisc2", "allow-all"]
   end
 
   config.vm.provision "shell",
--- provision.sh
+++ provision.sh
@@ -19,14 +19,76 @@ sudo apt install -y \
   software-properties-common \
   vim
 
+## Install Docker for IPv6.
+sudo groupadd docker
+if getent passwd vagrant; then
+  sudo usermod -aG docker vagrant
+fi
+if getent passwd ubuntu; then
+  sudo usermod -aG docker ubuntu
+fi
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "ipv6": true,
+  "fixed-cidr-v6": "2001:db8:1::/64"
+}
+EOF
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+## Disable systemd-resolved and install resolv.conf.
+##
+## This is only necessary to avoid a collision on
+## udp/53 between systemd-resolved and docker-bind64.
+sudo systemctl disable systemd-resolved.service
+sudo systemctl stop systemd-resolved.service
+sudo cp /etc/resolv.conf /etc/resolv.conf.$$
+if [ -e /vagrant/resolv.conf.custom ]; then
+  resolvconf=/vagrant/resolv.conf.custom
+else
+  resolvconf=/vagrant/resolv.conf
+fi
+sudo cp -f "${resolvconf}" /etc/resolv.conf
+
+## Install and start docker-bind64.
+sudo curl -fsSL https://raw.githubusercontent.com/josdotso/docker-bind64/master/bind64.service \
+  -o /etc/systemd/system/bind64.service
+sudo sed -i 's@53:@1053:@g' /etc/systemd/system/bind64.service  ## resolve port conflict with LXD dnsmasq
+sudo systemctl daemon-reload
+sudo systemctl enable bind64.service
+sudo systemctl start bind64.service
+sleep 20  # Give it a little time to pull image and start.
+sudo systemctl status bind64
+
+## Install and start docker-jool.
+sudo curl -fsSL https://raw.githubusercontent.com/josdotso/docker-jool/master/jool.service \
+  -o /etc/systemd/system/jool.service
+sudo systemctl daemon-reload
+sudo systemctl enable jool.service
+sudo systemctl start jool.service
+sleep 20  # Give it a little time to pull image and start.
+sudo systemctl status jool
+
 ## Initialize LXD with Preseed YAML heredoc.
 cat /vagrant/lxd.yaml | sudo lxd init --preseed
 
-## Launch LXD container for DNS64, NAT64: "n64"
-lxc launch --profile n64 ubuntu:18.04 n64
+## Add ubuntu-minimal LXD remote.
+lxc remote add --protocol simplestreams ubuntu-minimal https://mirrors.servercentral.com/ubuntu-cloud-images/minimal/releases/
 
-## Launch LXD container for Kubernetes Master #1: master1
-lxc launch --profile master1 ubuntu:18.04 master1
+## Launch LXD container for Kubernetes Master #1: "master1"
+lxc launch --profile master1 ubuntu-minimal:18.04 master1
 
 ## Report that it kind of worked.
 echo OK
